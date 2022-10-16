609717ea7191ad0019e620416a436c8ef975b25b
dokutoku <3729541-dokutoku@users.noreply.gitlab.com>
2022-10-16 15:01:36 +0900

    Fix DllMain

--- source/jansson_d/dll.d
+++ source/jansson_d/dll.d
@@ -4,13 +4,12 @@ module jansson_d.dll;
 version (Windows):
 version (JANSSON_D_DYNAMIC_LIBRARY):
 
-private static import core.sys.windows.basetsd;
 private static import core.sys.windows.windef;
 private static import core.sys.windows.winnt;
 
 extern (Windows)
 pure nothrow @safe @nogc @live
-export core.sys.windows.windef.BOOL DllMain(core.sys.windows.basetsd.HANDLE hModule, core.sys.windows.windef.DWORD reasonForCall, core.sys.windows.winnt.LPVOID lpReserved)
+public core.sys.windows.windef.BOOL DllMain(core.sys.windows.windef.HINSTANCE hModule, core.sys.windows.windef.DWORD fdwReason, core.sys.windows.winnt.LPVOID lpvReserved)
 
 	do
 	{
